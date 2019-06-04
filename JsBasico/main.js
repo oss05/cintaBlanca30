e354c0a39ac81fa2943b9fe65780227ddf24ef15
Ricardo <ricardokelebra@gmail.com>
2019-06-04 10:02:09 -0500

    JavaSvript basico0

--- JsBasico/main.js
+++ JsBasico/main.js
@@ -1,9 +1,89 @@
-let frutas = ['manzada', 'pera']
-console.log(frutas)
+// alert("Hola, si funciono!!!!")
 
-frutas.push('Banana')
-console.log(frutas)
+// let numero = 10
+// let texto = "Hola!"
+// let booleano = true
+// let array = []
+// let object = {}
 
-let last =frutas.pop('Banana')
+// + - / * %
 
-console.log(frutas)
+// let num1 = 100
+// let num2 = 500
+
+// //let result = num1 + num2
+
+// console.log(num1 + num2)
+
+// console.log(numero)
+
+
+// let saludo = "Hola!"
+// let name = "Pepe"
+// console.log(saludo)
+// console.log(name)
+
+// console.log( saludo + " " + name)
+
+// let entra = prompt("Ingrese su nombre")
+
+// console.log(entra)
+
+//________________
+//Challenge:
+// Van a preguntar al usuario Nombre, Direccion, año de nac. y con ese su edad
+// y mostrar en la consola en una linea
+
+// let name = prompt("Nombre")
+// let direc = prompt("Canton")
+// let año = prompt("Año de Nacimiento")
+// let act = 2019 - año
+
+// console.log("Nombre: "+ name + " "+ "Direccion: " + direc + " " +"Edad: "+ act )
+
+
+//Arreglos:
+
+// let array = [10, 20, 50 , 100]
+// let animals = ["perrito", "gatito", "perico"]
+
+// animals[3] = "hipopotamo"
+
+// animals.push("cocodrilo")
+// animals.push("nutria")
+
+// // animals.pop()
+
+// let str = "Hola mundo , como estas"
+// let search = str.indexOf("mundo")
+
+// console.log(search)
+
+
+// console.log(animals)
+
+//  let animals = ["perrito", "gatito", "perico"]
+
+//  animals.splice(0,1)
+
+//  console.log(animals)
+
+
+// Hacer un arreglod de  5 colores y añadir 2 con push
+// let colors = ["negro","blanco","verder","rosa","azul"]
+
+// colors.push("gris")
+// colors.push("morado")
+// console.log(colors)
+
+//Hacer un arreglo de 3 G.M, sumar 3 con push, y borrae el elemento 0 y 1
+
+let gm = ["perreke", "banda","duranguense"]
+
+gm.push("merol")
+gm.push("indie")
+gm.push("house")
+
+gm.splice(0,2)
+
+console.log(gm)
