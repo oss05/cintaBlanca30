23d177a5ba247b78f7d5d639d9a02d74a57be28a
Ricardo <ricardokelebra@gmail.com>
2019-06-05 10:53:03 -0500

    Ciclos, ppt

---
+++ 07_Ciclos/ciclos.js
@@ -0,0 +1,53 @@
+//Ciclo For
+
+for(let i = 0; i <= 100; i = i +1){
+    //console.log(i)
+}
+// ciclo del 500 al 1000 de 2 en 2
+for(let e = 500; e <= 1000; e = e+2){
+    //console.log(e)
+}
+// ciclo del 100 al 0 de -3 
+for(let r = 100;r >= 0; r = r -3){
+    // console.log(r)
+}
+// cilo de 1 al 50 de 10 en 10
+// for(let t = 0; t<=50; t =t+ 10 ){
+//     console.log(t)
+// }
+
+
+//Challenge---------------------------->
+// usando el ciclo for, evalular los numeros del 1-100 y decir si cuales son pares e impares
+
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i + " es par ")
+//     }else{
+//         console.log(i + " es impar ")
+//     }
+// }
+
+//Numero del 1-100
+// Si son divisibles entre 3 -> Fizz
+// Si son divisibles entre 5 -> Buzz
+// Si sin divisibles entre 5 y 3 -> FizzBuzz
+
+// for(let i = 0; i <= 100; i = i +1){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i + " FizzBuzz")
+//     }else if (i % 5 === 0){
+//             console.log(i + " Buzz")
+//     }else if(i % 3 === 0){
+//         console.log(i + " Fizz")
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+let array = ["a","b","c","d","e","f","g","h"]
+
+for(let i = 0; i < array.length ; i ++){
+    console.log(array[i])
+
+}
---
+++ PPT/main.js
@@ -0,0 +1,49 @@
+//Preguntar al usuario un numero, evaluar si es par o impar, mostrarlo en la consola
+
+// let numero = prompt("Ingresa un numero")
+
+// if(numero % 2 === 0){
+//     console.log("Es par :)")
+// }else{
+//     console.log("Es impar :(")
+// }
+
+// if(numero / 2 === 0){
+//     console.log("Es par :)")
+// }else{
+//     console.log("Es impar :(")
+// }
+
+// jugador1 = prompt("Player 1 : Escribe piedra, papel o tijera");
+// jugador2 = prompt("Player 2 : Escribe piedra papel o tijera");
+
+// if (jugador2 === "piedra" || jugador2 === "papel" || jugador2 === "tijera" && jugador1 === "piedra" || jugador1 === "papel" || jugador1 === "tijera" ){
+//     if (jugador1 === "piedra") {
+//         if (jugador2 === "papel") {
+//             console.log("Papel le gana a piedra. Ganó el jugador 2.")
+//         } else if (jugador2 === "piedra") {
+//             console.log("Hubo un empate")
+//         } else{
+//             console.log("Piedra le gana a tijera. Ganó el jugador 1.")
+//         }
+//     } else if (jugador1 === "papel") { 
+//         if (jugador2 === "tijera") {
+//             console.log("Tijera le gana a papel. Ganó el jugador 2.")
+//         } else if (jugador2 === "papel") {
+//             console.log("Hubo un empate")
+//         } else{
+//             console.log("Papel le gana a piedra. Ganó el jugador 1")
+//         }
+//     } else if (jugador1 === "tijera"){
+//         if (jugador2 === "piedra") {
+//             console.log("Piedra le gana a tijera. Ganó el jugador 2")
+//         } else if (jugador2 === "tijera") {
+//             console.log("Hubo un empate.")
+//         } else{
+//             console.log("Tijera le gana a papel. Ganó el jugador 1")
+//         }
+//     } 
+// } else {
+//     console.log("Ambos jugadores necesitan escribir piedra, papel o tijera")
+// }
+
